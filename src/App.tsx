050b978a36e395113e79adb889681d5bd6cf2076
Excellent_Torch <ishanmaduranga42@gmail.com>
2025-03-18 00:03:20 +0000

    Padding/Margin Adjustments + Content Added

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,8 @@ const App: React.FC = () => {
             <h1>My Name is Ishan Madhuranga,</h1>
             <h2>AI/Game Developer</h2>
             <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              I am an AI and machine learning enthusiast who specializes in developing unique solutions with cutting-edge technologies. 
+              With hands-on experience designing multiplayer games using Unreal Engine 5, I enjoy pushing the boundaries of interactive and AI-powered experiences.
             </p>
           </div>
           <div className="image-content">
