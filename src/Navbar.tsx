dd611083b27f9c85049be1d8eed945f9fe8561f1
Excellent_Torch <ishanmaduranga42@gmail.com>
2025-03-30 19:31:53 +0100

    Path Fix - 2

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
         <img src={menuIcon} alt="Menu Icon" />
       </div>
       <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
-        <li><a href="#/home" className="navbar-button">Home</a></li>
+        <li><a href="/" className="navbar-button">Home</a></li>
         
         <li><a href="#/Projects" className="navbar-button">Projects</a></li>
         <li><a href="#contact" className="navbar-button">Contact</a></li>
